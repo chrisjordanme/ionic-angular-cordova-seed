f165276b550b49c12ed1f105fd6d5a275524724a
Chris Jordan <cljordan1@gmail.com>
2014-01-23 12:04:29 -0500

    Stripped starter project down

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,10 @@
+'use strict';
+
 angular.module('starter.controllers', [])
+    .controller('PetIndexCtrl', function($scope) {
 
+    })
 
-// A simple controller that fetches a list of data from a service
-.controller('PetIndexCtrl', function($scope, PetService) {
-  // "Pets" is a service returning mock data (services.js)
-  $scope.pets = PetService.all();
-})
+    .controller('PetDetailCtrl', function($scope, $stateParams) {
 
-
-// A simple controller that shows a tapped item's data
-.controller('PetDetailCtrl', function($scope, $stateParams, PetService) {
-  // "Pets" is a service returning mock data (services.js)
-  $scope.pet = PetService.get($stateParams.petId);
-});
+    });
